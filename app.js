d0c3fc3a661fb506a2ec2fa2cd5d46f12c91daa9
Andrew Cope <andrew@thecope.net>
2017-11-08 17:02:59 -0800

    Update test for JSON

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ app.get('/mansplain', (req, res) => {
   var text = req.query.text;
   if (text.includes('actually')) {
     var result = {"isMansplaining":true};
-    console.log(result);
-    res.send(result.stringify);
+    res.send(result);
   } else {
     var result = {"isMansplaining":false};
-    console.log(result);
-    res.send(result.stringify);
+    res.send(result);
   }
   
 });
 
 app.get('/', (req, res) => res.send('<html>Hello World</html>'))
 
-app.get('/test', (req, res) => { console.log("{hello test"); res.send("{ 'hello test':true }"); })
+app.get('/test', (req, res) => { console.log("{hello test"); res.send({ 'hello test':true }); })
 
 app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
